740203f8b066c90ebc0f1f0bf7da77a837ec2380
yeison <yeisonarrieta0@gmail.com>
2025-02-14 23:04:26 -0500

    ahora si el final

--- src/Components/Texto14.jsx
+++ src/Components/Texto14.jsx
@@ -1,31 +1,34 @@
-import React from 'react'
+import React from "react";
 
 export const Texto14 = () => {
   return (
     <div>
-        <section className="h-[40rem] bg-imagen8 bg-cover bg-center flex items-center justify-center ">
-          <h1 className=" font-Raleway font-semibold	 text-transparent bg-gradient-to-r from-rose-500 via-rose-600 to-rose-500 bg-clip-text text-4xl sm:text-9xl  ">
-            FELIZ 14 DE FEBRERO
-          </h1>
-        </section>
-        <section className=" px-28 py-24 bg-[#EEDBBF]  border-b-2 border-[#67191F]">
-          <h1 className=" text-4xl sm:text-6xl pb-14 font-Raleway text-[#67191F] ">
-            ¿Puedes adivinar quien escribio esto ???
-          </h1>
-          <p className="text-xl sm:text-2xl font-Raleway ">
-            Poco sé de la noche pero la noche parece saber de mí, y más aún, me
-            asiste como si me quisiera, me cubre la conciencia con sus
-            estrellas. Tal vez la noche sea la vida y el sol la muerte. Tal vez
-            la noche es nada y las conjeturas sobre ella nada y los seres que la
-            viven nada. Tal vez las palabras sean lo único que existe en el
-            enorme vacío de los siglos que nos arañan el alma con sus recuerdos.
-            Pero la noche ha de conocer la miseria que bebe de nuestra sangre y
-            de nuestras ideas. Ella ha de arrojar odio a nuestras miradas
-            Sabiéndolas llenas de intereses, de desencuentros. Pero sucede que
-            oigo a la noche llorar en mis huesos. Su lágrima inmensa delira y
-            grita que algo se fue para siempre Alguna vez volveremos a ser
-          </p>
-        </section>
+      <section
+        className="h-[40rem] bg-cover bg-center flex items-center justify-center"
+        style={{ backgroundImage: "url('/assets/imagen8.jpeg')" }}
+      >
+        <h1 className="font-Raleway font-semibold text-transparent bg-gradient-to-r from-rose-500 via-rose-600 to-rose-500 bg-clip-text text-4xl sm:text-9xl">
+          FELIZ 14 DE FEBRERO
+        </h1>
+      </section>
+      <section className=" px-28 py-24 bg-[#EEDBBF]  border-b-2 border-[#67191F]">
+        <h1 className=" text-4xl sm:text-6xl pb-14 font-Raleway text-[#67191F] ">
+          ¿Puedes adivinar quien escribio esto ???
+        </h1>
+        <p className="text-xl sm:text-2xl font-Raleway ">
+          Poco sé de la noche pero la noche parece saber de mí, y más aún, me
+          asiste como si me quisiera, me cubre la conciencia con sus estrellas.
+          Tal vez la noche sea la vida y el sol la muerte. Tal vez la noche es
+          nada y las conjeturas sobre ella nada y los seres que la viven nada.
+          Tal vez las palabras sean lo único que existe en el enorme vacío de
+          los siglos que nos arañan el alma con sus recuerdos. Pero la noche ha
+          de conocer la miseria que bebe de nuestra sangre y de nuestras ideas.
+          Ella ha de arrojar odio a nuestras miradas Sabiéndolas llenas de
+          intereses, de desencuentros. Pero sucede que oigo a la noche llorar en
+          mis huesos. Su lágrima inmensa delira y grita que algo se fue para
+          siempre Alguna vez volveremos a ser
+        </p>
+      </section>
     </div>
-  )
-}
+  );
+};
